d3f241b48cc5d74e3bef75111c19978735f68907
Ioannis Bourlakos <jbourlakos@gmail.com>
2016-10-30 16:59:58 +0200

    Parameters, components and functions refactored
    Changed TOC version to 7.1-3

--- SimpleQuestCounter.lua
+++ SimpleQuestCounter.lua
@@ -1,68 +1,95 @@
-local questMaxLimit = 25
-local questMaxLimitColor = { 255/255, 94/255, 0/255} -- red/orange
-local questMinLimitColor = { 255/255, 255/255, 255/255} -- pure white
-
-----
+---
+-- Addon's parameters
 ----
 
 
-local parent = WorldMapFrame.BorderFrame --WorldMapDetailFrame--WorldMapFrame
-SimpleQuestCounter_Frame = CreateFrame("Frame", nil, parent)
-
-local frame = SimpleQuestCounter_Frame
-
-frame.counterFontString = frame:CreateFontString(nil,"ARTWORK", "GameFontHighlightSmall")
-local counterFontString = frame.counterFontString
-counterFontString:SetTextColor(unpack(questMinLimitColor),1)
--- set string
-counterFontString:SetFormattedText("Quests: ? / ?")
-
--- position in parent
-counterFontString:SetAllPoints(true)
-local verticalOffset = (-1)*3.25*counterFontString:GetHeight()
-local horizontalOffset = (-1)*counterFontString:GetWidth()/1.89
-counterFontString:SetPoint("TOPRIGHT", parent, "TOPRIGHT", horizontalOffset, verticalOffset)
-
--- font size: double than the default for the parent frame
-local fontFileName, previousFontHeight, flags = counterFontString:GetFont()
-local fontHeight = previousFontHeight * 2
-counterFontString:SetFont(fontFileName, fontHeight, flags)
-
--- shadow
-counterFontString:SetShadowColor(0, 0, 0, 0.75)
-counterFontString:SetShadowOffset(fontHeight*0.15,-fontHeight*0.15)
-
-frame:SetSize(counterFontString:GetWidth(),counterFontString:GetHeight())
+local questMaxLimit = 25 -- TODO: is it provided by WOW API?
+local questMaxLimitColor = { 255/255, 94/255, 0/255 } -- red/orange
+local questMinLimitColor = { 255/255, 255/255, 255/255 } -- pure white
+local fontTemplate = "GameFontHighlightSmall"
+local fontStringDepthLevel = "ARTWORK"
+local fontStringAlpha = 1 -- 0.0 (transparent) to 1.0 (opaque)
+local verticalOffsetToHeightFactor = 3.25 -- TODO: maybe rework it relative to fontheight and parent size
+local horizontalOffsetToWidthFactor = 1.89 -- TODO: maybe rework it relative to fontheight and parent size
+local shadowColor = {0, 0, 0, 0.75} -- {r,g,b,a}
+local shadowOffsetToFontHeightRatio = 0.15
+local fontStringTextFormat = "Quests: %d / %d" -- TODO: localize
 
 
--- on update => refresh
-frame:SetScript("OnUpdate", function (self, elapsed)
+----
+-- Functions
+----
+
+
+function SimpleQuestCounter_OnUpdate(self, elapsed)
     -- TODO: no need to refresh per frame
-        local frame = SimpleQuestCounter_Frame
-        local counterFontString = frame.counterFontString
-        local w = counterFontString:GetWidth()
-        local h = counterFontString:GetHeight()
+    local frame = SimpleQuestCounter_Frame
+    local counterFontString = frame.counterFontString
+    local w = counterFontString:GetWidth()
+    local h = counterFontString:GetHeight()
 
-        local _, questsNumber = GetNumQuestLogEntries()
-        counterFontString:SetFormattedText("Quests: %d / %d", questsNumber,questMaxLimit)
-        r,g,b = unpack(SimpleQuestCounter_calculateColor(questsNumber,questMaxLimit, questMinLimitColor, questMaxLimitColor))
-        counterFontString:SetTextColor(r,g,b,1)
-        SimpleQuestCounter_Frame:SetSize(w, h)
-    end
-)
+    local _, questsNumber = GetNumQuestLogEntries()
+    counterFontString:SetFormattedText(fontStringTextFormat, questsNumber,questMaxLimit)
+    r,g,b = unpack(SimpleQuestCounter_CalculateColor(questsNumber,questMaxLimit, questMinLimitColor, questMaxLimitColor))
+    counterFontString:SetTextColor(r,g,b, fontStringAlpha)
+    SimpleQuestCounter_Frame:SetSize(w, h)
+end
 
 
-frame:Show()
-counterFontString:Show()
 
-----
-----
-
-function SimpleQuestCounter_calculateColor(questsNumber, questMaxNumber, minLimitColor, maxLimitColor)
-    local result = {0,0,0}
+function SimpleQuestCounter_CalculateColor(questsNumber, questMaxNumber, minLimitColor, maxLimitColor)
+    local result = {}
     local ratio = questsNumber / questMaxNumber
     result[1] = math.abs(minLimitColor[1] - math.abs(minLimitColor[1] - maxLimitColor[1] ) * ratio * ratio * ratio)
     result[2] = math.abs(minLimitColor[2] - math.abs(minLimitColor[2] - maxLimitColor[2] ) * ratio * ratio * ratio)
     result[3] = math.abs(minLimitColor[3] - math.abs(minLimitColor[3] - maxLimitColor[3] ) * ratio * ratio * ratio)
     return result
 end
+
+
+----
+-- Components setup
+----
+
+
+-- the parent of the frame
+local parent = WorldMapFrame.BorderFrame --WorldMapDetailFrame--WorldMapFrame
+
+-- create frame
+SimpleQuestCounter_Frame = CreateFrame("Frame", nil, parent)
+local frame = SimpleQuestCounter_Frame
+
+-- create font string
+frame.counterFontString = frame:CreateFontString(nil, fontStringDepthLevel, fontTemplate)
+local counterFontString = frame.counterFontString
+counterFontString:SetTextColor(unpack(questMinLimitColor), fontStringAlpha)
+counterFontString:SetFormattedText(fontStringTextFormat, 99, 99) -- default text, to calculate initial dimensions
+
+-- position of font string in parent, relative to font string's size
+counterFontString:SetAllPoints(true)
+local verticalOffset = (-1) * verticalOffsetToHeightFactor * counterFontString:GetHeight()
+local horizontalOffset = (-1) * counterFontString:GetWidth() / horizontalOffsetToWidthFactor
+counterFontString:SetPoint("TOPRIGHT", parent, "TOPRIGHT", horizontalOffset, verticalOffset)
+
+-- font height: double than the default for the parent frame
+local fontFileName, previousFontHeight, flags = counterFontString:GetFont()
+local fontHeight = previousFontHeight * 2
+counterFontString:SetFont(fontFileName, fontHeight, flags)
+
+-- shadow
+counterFontString:SetShadowColor(unpack(shadowColor))
+local shadowXOffset = fontHeight * shadowOffsetToFontHeightRatio
+local shadowYOffset =  -fontHeight * shadowOffsetToFontHeightRatio
+counterFontString:SetShadowOffset(shadowXOffset, shadowYOffset)
+
+-- pack frame size to fontstring size
+frame:SetSize(counterFontString:GetWidth(),counterFontString:GetHeight())
+
+
+-- on update => refresh
+frame:SetScript("OnUpdate", SimpleQuestCounter_OnUpdate)
+
+-- show components
+frame:Show()
+counterFontString:Show()
+
